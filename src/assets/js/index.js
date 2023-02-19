a983d4a06820fdf4464226e0c7f2ea5a1c22b296
chayzat <chayzat.oorzhak.2001@mail.ru>
2023-02-19 22:30:52 +0300

    feat: add responsive header

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,19 @@
 import '../styles/styles.scss'
+
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close')
+
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+        navClose.classList.add('show-close')
+    })
+}
+
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+        navClose.classList.remove('show-close')
+    })
+}
